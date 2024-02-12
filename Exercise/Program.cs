c6de53ab1546b27a89de1d0018c3cfe199a5cfa7
henrymiessan <miessanhenri@gmail.com>
2024-02-12 12:47:01 +0000

    Initial commit

---
+++ Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿/**
+Exercise 1:affichage pesonalisé
+Author: Miessan henri
+Date: 12-02-24
+
+*/
+namespace Exercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+          string name="miessan";
+   string prenom="Henri";
+   int age = 30;
+
+//    le message à afficher
+
+   string message = $"bonjour{name} {prenom}, vous avez {age} ans";
+
+//    Afficher le message 
+
+  Console.WriteLine(message);
+    }
+}
---
+++ ExerciseA/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExerciseA;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    
+         // Prompt the user to enter their name
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        // Prompt the user to enter their last name
+        Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        // Prompt the user to enter their age
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        // Display the message based on user input
+        Console.WriteLine($"Good morning Mr. {lastName} {name}, you are {age} years old.");
+
+    }
+}
