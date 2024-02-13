df0e455a7314e13849a7aea72a3061d6221accb5
henrymiessan <miessanhenri@gmail.com>
2024-02-13 12:12:45 +0000

    Exerise4 termine

---
+++ ExerciseC/Program.cs
@@ -0,0 +1,35 @@
+﻿/**
+Exercice4:calcul daffichage;
+Author:Miessan Henri
+Date: 13-02-24
+
+*/
+namespace ExerciseC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        // Input the price excluding tax (net price)
+        Console.WriteLine("Enter the price excluding tax (net price):");
+        decimal netPrice = decimal.Parse(Console.ReadLine());
+
+        // Entrez  VAT rate
+        Console.WriteLine("Entrez the VAT rate (in percentage):");
+        decimal vatRate = decimal.Parse(Console.ReadLine());
+
+        // Calculate the VAT amount
+        decimal vatAmount = netPrice * (vatRate / 100);
+
+        // Calculate the total price including VAT
+        decimal totalPriceIncludingVAT = netPrice + vatAmount;
+
+        // Display the result
+        Console.WriteLine($"VAT Amount: {vatAmount}");
+        Console.WriteLine($"Total Price Including VAT: {totalPriceIncludingVAT}");
+
+
+    }
+}
