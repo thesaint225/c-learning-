a6290fbe035818782f362fac66a9ce5c254b89ad
henrymiessan <miessanhenri@gmail.com>
2024-02-15 13:30:14 +0000

    exercise 8 termine )

---
+++ Exercise_G/Program.cs
@@ -0,0 +1,46 @@
+﻿/**
+Exercise 8 :boucle while
+Author :Miessan Henri
+Date : 15-02-24
+Date :
+*/
+namespace Exercise_G;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double total = 0;
+        string userInput;
+
+        Console.WriteLine("Avez-vous un prix à saisir? (yes/no)");
+
+        // Read user input and validate
+        userInput = Console.ReadLine().ToLower();
+
+        while (userInput == "yes")
+        {
+            Console.Write("Saisir un prix: ");
+            double price = double.Parse(Console.ReadLine());
+            total += price;
+
+            Console.WriteLine("Avez-vous un autre prix à saisir? (yes/no)");
+
+            // Read user input and validate
+            userInput = Console.ReadLine().ToLower();
+
+            // Check if userInput is neither "yes" nor "no"
+            while (userInput != "yes" && userInput != "no")
+            {
+                Console.WriteLine("Veuillez entrer 'yes' ou 'no' uniquement.");
+                userInput = Console.ReadLine().ToLower();
+            }
+        }
+
+        // Display total price if user input is "no"
+        if (userInput == "no")
+        {
+            Console.WriteLine($"Total price: {total}");
+        }
+    }
+}
