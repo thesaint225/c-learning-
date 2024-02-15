a21a8c523206f44fe69201ee8f1a6f72accc5018
henrymiessan <miessanhenri@gmail.com>
2024-02-15 00:02:17 +0000

    Exercise6

---
+++ Exercise_E/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Exercise_E;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Declaration
+        int moyenne;
+
+        Console.Write("Entrez votre moyenne :");
+
+        // read the input from the user as a string
+
+        string input = Console.ReadLine();
+
+        // Convert the string input to an integer
+
+        try
+        {
+            // Convert the string input to an integer
+            moyenne = int.Parse(input);
+
+            if (moyenne >= 16)
+            {
+                Console.WriteLine("Très bien !");
+            }
+            else if (moyenne <= 15)
+            {
+
+                Console.WriteLine("bien ");
+
+            }
+            else if (moyenne <= 13)
+            {
+                Console.WriteLine("assez-bien: ");
+
+            }
+            else if (moyenne <= 11)
+            {
+                Console.WriteLine("passable ");
+
+            }
+            else
+            {
+                Console.WriteLine("recallez ");
+
+            }
+
+
+
+
+
+
+        }
+        catch (FormatException)
+        {
+
+            Console.WriteLine("Please enter a valid integer.");
+
+        }
+
+
+
+    }
+}
