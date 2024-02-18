c0d96380a693d3a614f1a5ca69ea36e2efd4bc5f
henrymiessan <miessanhenri@gmail.com>
2024-02-18 08:57:15 +0000

    Exerise N9:boucle (do/while)

---
+++ Exercise_H/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise_H;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int grade;
+
+        // Prompt the user to enter a grade
+        Console.WriteLine("Enter a grade (between 0 and 20):");
+
+        // Read the input and validate it using a do-while loop
+        do
+        {
+            // Read the grade from the user
+            Console.Write("Grade: ");
+            grade = int.Parse(Console.ReadLine());
+
+            // Check if the grade is within the valid range
+            if (grade < 0 || grade > 20)
+            {
+                Console.WriteLine("Please enter a grade between 0 and 20.");
+            }
+        } while (grade < 0 || grade > 20);
+
+        // Display the correct grade entered
+        Console.WriteLine($"Correct grade entered: {grade}");
+
+    }
+}
---
+++ Exercise_I/Program.cs
@@ -0,0 +1,40 @@
+﻿/**
+Exerise N9:boucle (do/while)
+Author :Miessan
+Date:18-02-2024
+*/
+namespace Exercise_I;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //    declarations
+        char sex;
+        // prompt the user to enter  their sex(f/m)
+        Console.Write("Enter your sex (f/m)");
+
+        //  CHECK THE SEX
+
+        do
+        {
+            // read the sex from the user
+            Console.Write("Sex: ");
+            sex = char.ToLower(Console.ReadKey().KeyChar);
+            //  check if the input is  valide("m" or f")
+            if (sex != 'm' && sex != 'm')
+            {
+                Console.WriteLine("\nPlease enter 'm'or 'f'");
+            };
+
+
+
+        } while (sex != 'm' && sex != 'f');
+        // Affichage du message 
+        if (sex == 'm')
+        {
+            Console.WriteLine("\n Good morning Sir ");
+        }
+        else { Console.WriteLine("Good Morning Madam"); }
+    }
+}
