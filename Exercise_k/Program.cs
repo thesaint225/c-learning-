f35042988cfb60b12bbb663d8737f4283d22aea5
henrymiessan <miessanhenri@gmail.com>
2024-02-20 09:14:15 +0000

    Exercise 12: boucle for

---
+++ Exercise_k/Program.cs
@@ -0,0 +1,21 @@
+﻿/**
+Exercise 12: boucle for
+Author:Miessan henri
+Date : 20-02-24
+*/
+
+namespace Exercise_k;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // display of mutliplication of 3
+        for (int i = 0; i <= 10; i++)
+        {
+            int result = 3 * i;
+            Console.Write($"3 * {i}={result}");
+            Console.ReadLine();
+        }
+    }
+}
