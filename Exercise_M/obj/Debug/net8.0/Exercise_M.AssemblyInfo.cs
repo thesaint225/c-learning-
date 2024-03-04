7eee866fff72e26afe84ae25ee462900957874a7
henrymiessan <miessanhenri@gmail.com>
2024-03-04 13:17:10 +0000

    Exercise_N Added

--- Exercise_M/obj/Debug/net8.0/Exercise_M.AssemblyInfo.cs
+++ Exercise_M/obj/Debug/net8.0/Exercise_M.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise_M")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4943577e43857610076dfc79134eed6c9cb86a1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d8d9b8f22280290f5bd800095d1043d3a5ca6d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise_M")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise_M")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
